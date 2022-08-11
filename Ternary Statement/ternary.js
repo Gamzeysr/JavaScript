0b2b999dce9f28760f303dff46e1b0cfd46f8cd3
Gamze <gamzeyasar35@icloud.com>
2022-08-11 13:19:29 +0300

    ternary

---
+++ Ternary Statement/ternary.js
@@ -0,0 +1,25 @@
+// condition ? Expression1 : expression2
+// Burada condition true ise expression1 çalışacak
+    //    conditon false ise expression2 çalışacak
+
+    // AND(&&) operatörü iki tane True varsa True döndürür diğerleri false döner
+// const first = 0;
+// const second = 20;
+// const third = 30;
+// let result = first && second && third;
+// console.log(result);
+
+// const userLoggedIn = true;
+// userLoggedIn && console.log('Welcome'); 
+     
+
+// OR(||)  ifadesi her ikiside False a false döndürür. 
+// play with values
+
+// const first = 0;
+// const second = 20;
+// const third = 30;
+// let result = first || second || third;
+// console.log(result);
+
+
