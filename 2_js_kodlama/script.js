c5277881255f1320cc9258bb9dcf1c2add82ec70
Gamze <gamzeyasar35@icloud.com>
2022-08-15 13:44:51 +0300

    object

--- 2_js_kodlama/script.js
+++ 2_js_kodlama/script.js
@@ -1,2 +1,29 @@
-console.log(5000 +(5000*0.3)) // maaş ali
-console.log(6000 +(6000 * 0.3)) // maaş can
+// console.log(5000 +(5000*0.3)) // maaş ali
+// console.log(6000 +(6000 * 0.3)) // maaş can
+
+
+// let a = prompt('Bir sayı girinşiz');
+// let b = prompt('sayı giriniz')
+// 
+// console.log(a*b);
+
+
+
+// let x = + prompt('Bir sayı giriniz')
+// let y = + prompt('Bir sayı giriniz')
+// let area = x*y
+// console.log(`dikdörtgenin alanı: ${area}`)
+
+// let a = +prompt("bir sayı giriniz");
+// 
+// 
+// 
+// let a= prompt("Lütfen bir sayı giriniz");
+// let b= prompt("Lütfen bir sayı giriniz");
+// let c= prompt("Lütfen bir sayı giriniz");
+// let g= (a1%10)==(b%10)==(c%10);
+// console.log(g);
+
+let a = 5;
+
+console.log(a);
